da42100d7101d800f049b48555105075c453129c
andrewfecenko <andrewfecenko@rogers.com>
2016-10-21 09:46:18 -0400

    Update loadit_aws.sql

--- loadit_aws.sql
+++ loadit_aws.sql
@@ -1,5 +1,8 @@
+-- Connect to our database
 connect to wow ;
 
+-- Load the Reddit Data from S3 into our comments table using our alias.
 load from DB2REMOTE://S3ObjectStore//redditcomments.csv of del insert into comments ;
 
+--Confirm that the load was successful by counting the rows.
 select count(*) from comments ;
